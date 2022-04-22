8a0e08329ea527b40d6cb32241017671c4db5b0a
Pascal Fares <pascal.fares@cofares.net>
2022-04-22 11:24:29 +0300

    Initialisation d'un projet SpringBoot

---
+++ DemoOzlem/src/main/java/net/cofaret/DemoOzlemApplication.java
@@ -0,0 +1,13 @@
+package net.cofaret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOzlemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOzlemApplication.class, args);
+	}
+
+}
---
+++ DemoOzlem/src/test/java/net/cofaret/DemoOzlemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cofaret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOzlemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
